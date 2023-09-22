28ec4d19ee114ddbe9da8f917f98bc0d89e680b6
Ayomil <126260714+Ayomil@users.noreply.github.com>
2023-09-22 12:58:18 +0100

    New Commit

--- src/Components/PhotoGallery.jsx
+++ src/Components/PhotoGallery.jsx
@@ -26,6 +26,7 @@ const DraggablePhoto = ({ photo, index, movePhoto }) => {
       ref={(node) => ref(drop(node))}
       style={{ cursor: "move" }}
     >
+      <h3 style={{textAlign:"center"}}>{`Photo ${index}`}</h3>
       <img className="photo-image" src={photo} alt={`Photo ${index}`} />
     </div>
   );
@@ -53,26 +54,28 @@ const PhotoGallery = () => {
   return (
     <>
       <div>
-        <h2 style={{ textAlign: "center" }}>Photo Gallery</h2>
+        <div style={{display:"flex", flexDirection:"row"}}>
+          <h2>Photo Gallery</h2>
+          <input placeholder="Search Image" className="searchI"></input>
+        </div>
         <hr />
         <DndProvider backend={HTML5Backend}>
-          
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-evenly",
-              }}
-            >
-              {photos.map((photo, index) => (
-                <DraggablePhoto
-                  key={index}
-                  photo={photo}
-                  index={index}
-                  movePhoto={movePhoto}
-                />
-              ))}
-            </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-evenly",
+            }}
+          >
+            {photos.map((photo, index) => (
+              <DraggablePhoto
+                key={index}
+                photo={photo}
+                index={index}
+                movePhoto={movePhoto}
+              />
+            ))}
+          </div>
         </DndProvider>
       </div>
     </>
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import PhotoGallery from "./Components/PhotoGallery";
 
 function Actual() {
-    const [selectedImg, setSelectedImg] = useState(null);
+  
   return (
     <>
       <PhotoGallery/>
